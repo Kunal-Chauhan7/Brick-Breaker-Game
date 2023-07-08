11be705e8b1ed9c55991c0a6a1cb60b8de25f34c
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-07-09 01:58:37 +0530

    made Frame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame obj = new JFrame();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
